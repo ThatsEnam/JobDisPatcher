4ba12c54458ad0b78ebffee2d7cbf4c1b6ed2397
Thats Enam <you@example.com>
2017-07-05 18:41:10 +0600

    jgjhgj

--- app/src/main/java/com/evolutioninc/jobdispatcher/Job.java
+++ app/src/main/java/com/evolutioninc/jobdispatcher/Job.java
@@ -24,35 +24,6 @@ public class Job extends JobService {
         Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         vibrator.vibrate(2000);
         Toast.makeText(this, "Job Executing........"+params.getExtras().getString("sec"), Toast.LENGTH_LONG).show();
-        NotificationCompat.Builder mBuilder =
-                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ring)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!"+params.getExtras().getString("sec"));
-// Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, MainActivity.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-// Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(MainActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-        mNotificationManager.notify(1, mBuilder.build());
-/*       That's it. Your user has now been notified.
-        Applying an expanded layout to a noti*/
         return true;
     }
 
